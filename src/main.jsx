439a444458f9da8029dd825f5b1aa121e68be37e
RaulRomero26 <link_2642@hotmail.com>
2023-02-28 13:00:12 -0600

    update: hash router

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 
 import { ArgosApp } from './ArgosApp';
 import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <ArgosApp />
-    </BrowserRouter>
+    </HashRouter>
   // </React.StrictMode>
 );
