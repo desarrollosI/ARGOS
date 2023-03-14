8d6d17d0355b1afd5f59f97c536d1d5527c916f2
RaulRomero26 <link_2642@hotmail.com>
2023-03-14 17:30:20 -0600

    update: pagina historico

--- src/argos/pages/HistoricoPage.jsx
+++ src/argos/pages/HistoricoPage.jsx
@@ -1,6 +1,25 @@
+import React from 'react'
+
+import { useParams } from 'react-router-dom';
+import { useFetch } from '../../hooks/useFetch';
+import { LoadingSpinner, Historico } from '../components/';
 
 export const HistoricoPage = () => {
+
+  const { folio } = useParams();
+  const { data, isLoading, hasError } = useFetch(`http://172.18.10.71:9090/api/base/historico`, `POST`, JSON.stringify({ folio: folio.trim() }));
+  console.log('despues del fetch: ', data)
+
+
+
   return (
-    <div>HistoricoPage</div>
+    <>
+      {/* recuerda, el is loading debe de ir en las condicionales, o react se sigue como secuencial */}
+      {
+        (isLoading)
+          ? <LoadingSpinner />
+          : <Historico data={data.data.Historico}/>
+      }
+    </>
   )
 }
