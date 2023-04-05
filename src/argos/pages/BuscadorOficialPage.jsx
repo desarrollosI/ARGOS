2a84d1adad5b1567cce2a3b2d18a01b595a098db
RaulRomero26 <link_2642@hotmail.com>
2023-04-05 17:59:37 -0600

    update: inicio componente filtro ine

--- src/argos/pages/BuscadorOficialPage.jsx
+++ src/argos/pages/BuscadorOficialPage.jsx
@@ -3,7 +3,28 @@ import React from "react";
 import { JumpToTop } from "../components/Shared/JumpToTop";
 import { SelectBaseComponent } from "../components/Table";
 
+import { useForm } from '../../hooks';
+import { personasApi } from "../../api";
+
+const personaFormFields = {
+  nombre:    '',
+  paterno: '',
+  materno: ''
+}
+
 export const BuscadorOficialPage = () => {
+
+  const { nombre, paterno, materno, onInputChange:onSearchInputChange } = useForm( personaFormFields );
+
+  const buscarPersona = async ( event ) => {
+      event.preventDefault();
+      console.log({ nombre,paterno,materno });
+
+      const res = await personasApi.post('/puebla',{ nombre,paterno,materno })
+      console.log(res)
+
+  }
+
   return (
     <>
       <div className="container-fluid">
@@ -26,9 +47,55 @@ export const BuscadorOficialPage = () => {
           </div>
           <hr />
         </div>
+
         <div className="row">
-          <SelectBaseComponent base={'PERSONAS'} /> 
+          <div className="col">
+          <form onSubmit={ buscarPersona }>
+                <div className="form-group mb-2">
+                    <input 
+                        type="text"
+                        className="form-control"
+                        placeholder="Nombre"
+                        name="nombre"
+                        value={ nombre }
+                        onChange={ onSearchInputChange }
+                    />
+                </div>
+                <div className="form-group mb-2">
+                    <input 
+                        type="text"
+                        className="form-control"
+                        placeholder="Apellido Paterno"
+                        name="paterno"
+                        value={ paterno }
+                        onChange={ onSearchInputChange }
+                    />
+                </div>
+                <div className="form-group mb-2">
+                    <input 
+                        type="text"
+                        className="form-control"
+                        placeholder="Apellido Ma"
+                        name="materno"
+                        value={ materno }
+                        onChange={ onSearchInputChange }
+                    />
+                </div>
+                
+                <div className="d-grid gap-2">
+                    <input 
+                        type="submit"
+                        className="btnSubmit"
+                        value="Buscar" 
+                    />
+                </div>
+            </form>
+          </div>
         </div>
+
+        {/* <div className="row">
+          <SelectBaseComponent base={'PERSONAS'} /> 
+        </div> */}
         {/*<div className="row">
           <SelectBaseComponent base={'SARAI INSPECCIONES'} /> 
         </div>*/}
