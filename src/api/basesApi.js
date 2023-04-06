e06539b1668fd8d29059f7e3aa1e981c1a5a268c
RaulRomero26 <link_2642@hotmail.com>
2023-04-06 15:57:04 -0600

    update comentarios en basesApi

--- src/api/basesApi.js
+++ src/api/basesApi.js
@@ -1,13 +1,17 @@
+//Se realizan las importaciónes necesarias para el funcionamiento de la función
 import axios from 'axios';
+//Se importa el helper que expone las variables de entorno
 import { getEnvVariables } from '../helpers';
-
+//Llamando a dicha funcion se obtiene la variable necesaria
 const { VITE_BACKEND_SERVER_BASES } = getEnvVariables()
-
+//Se crea y exporta la funcion que genera nuestro intermediario entre el frontend
+//y el backend
 export const basesApi = axios.create({
     baseURL: VITE_BACKEND_SERVER_BASES
 });
 
-
+//Con nuestra funcion creada se le añade la posibilidad de interceptar las solicitudes
+//del backend al servidor añadoendole los headers y el x-token necesario pra el backend
 basesApi.interceptors.request.use( config => {
 
     config.headers = {
@@ -18,7 +22,8 @@ basesApi.interceptors.request.use( config => {
     return config;
 })
 
-
+//De igual manera se añade el interceptor de las respuestas, para que en caso de respuestas
+// con status 400, manejadas por el servidor sacar al usuario no autenticado de la aplicación
  basesApi.interceptors.response.use(
      response => response,
      error => {
