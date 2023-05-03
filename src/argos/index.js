fe74cc9f5d0cc4a81dde2af7a3b6087a95849d25
RaulRomero26 <link_2642@hotmail.com>
2023-05-03 09:27:52 -0600

    update: comentado todo el modulo argos

--- src/argos/index.js
+++ src/argos/index.js
@@ -1,4 +1,3 @@
-
-
+//Archivo de barril para faciligar la exportacion de las paginas y las rutas
 export * from './pages';
 export * from './routes/ArgosRoutes';
--- src/argos/routes/ArgosRoutes.jsx
+++ src/argos/routes/ArgosRoutes.jsx
@@ -1,4 +1,9 @@
+/*
+  Este componente solamente es un aislante para las rutas internas de argos, y su redireccion hacia todas las paginas internas que
+  maneje el sistema, por eso la decisión de agruparlo en su carpeta routes.
 
+  El componente es meramente semántico
+*/
 import { NavbarN } from '../../ui';
 
 export const ArgosRoutes = () => {
