3f08dd5db1a84749c648b63f774b4563a748a3d5
RaulRomero26 <link_2642@hotmail.com>
2024-01-16 11:23:00 -0600

    update: fixed poligono

--- src/argos/helpers/Mapa/capaPerToExcel.js
+++ src/argos/helpers/Mapa/capaPerToExcel.js
@@ -29,10 +29,13 @@ export const capasPerToExcel = ({hechos,detencion,domicilio,inspecciones,siceven
       const fieldsSicEv = ['Folio_infra','Elemento_Captura','Folio_911','FechaHora_Captura','FechaHora_Recepcion','Zona','Vector','Colonia','Calle','Calle2','NoExt','CP','CoordX','CoordY','CSviolencia','Tipo_Violencia','FechaHora_Activacion','Fuente','Status_Cancelado','Status','Quien_Habilito','Id_Seguimiento','Conteo_Masculinos','Conteo_Femeninas','Conteo_Vehiculos','Status_Evento','ClaveSeguimiento','SeguimientoTerminado','hechos_concat','hechos_concathoras','delitos_concat','delito_giro','responsables_concat','Tipoarma_concat','Tipos_Vehiculos','vehiculos_involucrados','vehiculos_concat','entrevistas_seguimiento_concat','fotos_seguimiento_concat','Dia_semana','Hora_trunca','Hora_trunca2']
       const ws4 = XLSX.utils.json_to_sheet(siceventos, { header: fieldsSicEv }); // sheet
       XLSX.utils.book_append_sheet(wb, ws4, "Resultrados_Mapa_SicEventos"); //sheet name
+      
+      if(puntosidentificados.lenght){
 
-      const fieldsPI = Object.keys(puntosidentificados[0]);
-      const ws5 = XLSX.utils.json_to_sheet(puntosidentificados, { header: fieldsPI }); // sheet
-      XLSX.utils.book_append_sheet(wb, ws5, "Resultrados_Puntos"); //sheet name
+            const fieldsPI = Object.keys(puntosidentificados[0]);
+            const ws5 = XLSX.utils.json_to_sheet(puntosidentificados, { header: fieldsPI }); // sheet
+            XLSX.utils.book_append_sheet(wb, ws5, "Resultrados_Puntos"); //sheet name
+      }
 
       insertHistorial({lugar:'Geoanalisis',tipo:'Exportacion CSV', descripcion:'Exportacion datos de poligono personalizado'});
 
