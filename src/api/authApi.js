613fea5850a9e545103ddc47d3f4f29c7c81b103
RaulRomero26 <link_2642@hotmail.com>
2024-04-24 09:45:40 -0600

    update: urls dinamicas por ubicacion

--- src/api/authApi.js
+++ src/api/authApi.js
@@ -1,19 +1,26 @@
 import axios from 'axios';
 import { getEnvVariables } from '../helpers';
 
-const { VITE_BACKEND_SERVER_AUTH } = getEnvVariables()
+const { VITE_BACKEND_SERVER_AUTH, VITE_BACKEND_SERVER_AUTH_LOCAL } = getEnvVariables();
 
 export const authApi = axios.create({
     baseURL: VITE_BACKEND_SERVER_AUTH
 });
 
-// Todo: configurar interceptores
-authApi.interceptors.request.use( config => {
+// Configurar interceptores
+authApi.interceptors.request.use(config => {
+    const url = window.location.href;
 
-     config.headers = {
-         ...config.headers,
-         'x-token': localStorage.getItem('token')
-     }
+    if (url.includes('187.216.250.252')) {
+        config.baseURL = VITE_BACKEND_SERVER_AUTH;
+    } else if (url.includes('172.18.110.90')) {
+        config.baseURL = VITE_BACKEND_SERVER_AUTH_LOCAL;
+    }
+
+    config.headers = {
+        ...config.headers,
+        'x-token': localStorage.getItem('token')
+    };
 
     return config;
-})
+});
